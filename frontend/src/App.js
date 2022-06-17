230246441fb7e048c0b9f5d59a6e003e82f03c40
khaledHalhouli <101004067+khaledHalhouli@users.noreply.github.com>
2022-06-17 21:05:01 +0300

    merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,10 +12,8 @@ import Pay from "./component/paypal";
 import Getallcarts from "./component/getallcartadmin";
 import LOGIN from "./component/LOGIN";
 import ORDERSALE from "./component/product_used/create_order_sale";
-import ALLORDERSALE from "./component/product_used/all_order_sale"
+import ALLORDERSALE from "./component/product_used/all_order_sale";
 import ONEPRODUCTUSED from "./component/product_used/one_product_used";
-
-
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setAllproduct } from "./redux/reducers/search";
@@ -25,11 +23,8 @@ import ALLPRODUCTUSED from "./component/product_used/all_product_used";
 import ALLPRODUCTFORADMIN from "./component/product_used/all_order_sale_admin";
 import ADMINCONTROL from "./admin_control";
 import { CategoryBar } from "./component/CategoryBar/index";
-
 import Homepage from "./component/homepage";
-
 import { NavBar } from "./component/NavBar";
-
 import { ProductPage } from "./component/ProductPage/index";
 import CartPage from "./component/CartPage/index";
 import jwtDecode from "jwt-decode";
@@ -37,10 +32,7 @@ import FOOTER from "./component/FOOTER";
 import UserTable from "./component/UserTable/UserTable";
 import ProductTable from "./component/ProductTable";
 
-
-
 import { SubCatgoryPage } from "./component/SubCategoryPage/index";
-
 
 function App() {
   ///////////////////////////////search proccess//////////////////////////
@@ -75,45 +67,43 @@ function App() {
       })
       .catch((err) => {});
   }, []);
-  
+
   ////////////////////////////////////////////////////////////////////
 
-
-
-  
-  if(decodeToken("role")==1){
-    return(
-    <div className="App">
-    <div className="mainphotos">
-      <img className="mainphotosclass" src={photo}></img>
-    </div>
-    <NavBar />
-    <div className="routesdiv_admin">
-    
-      <ADMINCONTROL/>
-      <Routes>
-        <Route path="/admin/usersTable" element={<UserTable />} />
-        <Route path="/admin/productTable" element={<ProductTable />} />
-        <Route path="/creat" element={<Createprodact />} />
-        <Route path="/admin/uplodphoto" element={<Getphotosmain />} />
-        <Route path="/all_order_sale_for_admin" element={<ALLPRODUCTFORADMIN/>}/>
-      </Routes>
-    
-    </div>
-    <div className="mainphotos">
-      <img className="mainphotosclass" src={photo2}></img>
-    </div>
-  </div>
-    )
-  }else{
+  if (decodeToken("role") == 1) {
+    return (
+      <div className="App">
+        <div className="mainphotos">
+          <img className="mainphotosclass" src={photo}></img>
+        </div>
+        <NavBar />
+        <div className="routesdiv_admin">
+          <ADMINCONTROL />
+          <Routes>
+            <Route path="/admin/usersTable" element={<UserTable />} />
+            <Route path="/admin/productTable" element={<ProductTable />} />
+            <Route path="/creat" element={<Createprodact />} />
+            <Route path="/admin/uplodphoto" element={<Getphotosmain />} />
+            <Route
+              path="/all_order_sale_for_admin"
+              element={<ALLPRODUCTFORADMIN />}
+            />
+          </Routes>
+        </div>
+        <div className="mainphotos">
+          <img className="mainphotosclass" src={photo2}></img>
+        </div>
+      </div>
+    );
+  } else {
     return (
       <div className="App">
         <div className="mainphotos">
           <img className="mainphotosclass" src={photo}></img>
         </div>
         <div className="routesdiv">
-        <NavBar />
-        <CategoryBar />
+          <NavBar />
+          <CategoryBar />
           <Routes>
             <Route path="/rigester" element={<REGISTER />} />
             <Route path="/" element={<Homepage />} />
@@ -124,42 +114,30 @@ function App() {
             <Route path="/admin/uplodphoto" element={<Getphotosmain />} />
             <Route path="/category/:id/products" element={<GetProdact />} />
             <Route path="/resulsearch" element={<PAGEAllRESULTSEARCH />} />
-            <Route path="/category/product/:id" element={<ProductPage />}/>
+            <Route path="/category/product/:id" element={<ProductPage />} />
             <Route path="/cart/:id" element={<CartPage />} />
-            <Route path="/create_order_sale" element={<ORDERSALE/>} />
-            <Route path="/all_order_sale" element={<ALLORDERSALE/>} />
-            <Route path="/all_product_used" element={<ALLPRODUCTUSED/>} />
-            <Route path="/all_order_sale_for_admin" element={<ALLPRODUCTFORADMIN/>}/>
-            <Route path="/one_product_used/:id" element={<ONEPRODUCTUSED/>} />
-               <Route path="/admin/cart" element={<Getallcarts />} />
-               <Route
-                path="/subCategory/:subCategory_id"
-                element={<SubCatgoryPage />}/>
-             
-               
-
-       
+            <Route path="/create_order_sale" element={<ORDERSALE />} />
+            <Route path="/all_order_sale" element={<ALLORDERSALE />} />
+            <Route path="/all_product_used" element={<ALLPRODUCTUSED />} />
+            <Route
+              path="/all_order_sale_for_admin"
+              element={<ALLPRODUCTFORADMIN />}
+            />
+            <Route path="/one_product_used/:id" element={<ONEPRODUCTUSED />} />
+            <Route path="/admin/cart" element={<Getallcarts />} />
+            <Route
+              path="/subCategory/:subCategory_id"
+              element={<SubCatgoryPage />}
+            />
           </Routes>
-        <FOOTER />
+          <FOOTER />
         </div>
         <div className="mainphotos">
           <img className="mainphotosclass" src={photo2}></img>
         </div>
-
-      </div>)
-
-      }}
-          
-         
-
-   
-    
-
-
-
-
-
-
-
+      </div>
+    );
+  }
+}
 
 export default App;
